f658428f3bc5d5a653678212aed5a6def858965f
prajyotgorlewar <134780160+prajyotgorlewar@users.noreply.github.com>
2023-09-24 16:10:24 +0530

    update

--- My project (2)/Assets/Scripts/EndQuizHandler.cs
+++ My project (2)/Assets/Scripts/EndQuizHandler.cs
@@ -11,12 +11,15 @@ public class EndQuizHandler : MonoBehaviour
     [SerializeField] levelEnd lvlEndScript;
     public UnityEvent correctAnsScene;
     public UnityEvent incorrectAnsScene;
+    private int iq;
 
     public void CheckAnswer(TextMeshProUGUI textMesh)
     {
         if (textMesh.text == lvlEndScript.options[clueTextHandler.clueIndex][lvlEndScript.queAnsIndexes[clueTextHandler.clueIndex]])
         {
             correctAnsScene.Invoke();
+            iq += 5;
+            PlayerPrefs.SetInt("IQ", iq);
         }
         else
         {
