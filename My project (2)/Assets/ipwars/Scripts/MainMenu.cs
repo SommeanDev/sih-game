a36129dd3242bb1ccb41ab683c7f2ea3b3a581c4
prajyotgorlewar <134780160+prajyotgorlewar@users.noreply.github.com>
2023-10-09 07:41:20 +0530

    up26

--- My project (2)/Assets/ipwars/Scripts/MainMenu.cs
+++ My project (2)/Assets/ipwars/Scripts/MainMenu.cs
@@ -6,6 +6,39 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is call before the first frame update
+    public smove spaceship;
+    public GameObject tutorial;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("IPWarTutorial", 0) == 0)
+        {
+            ActivatePanel(tutorial);
+            spaceship.pause();
+            PlayerPrefs.SetInt("IPWarTutorial", 1);
+            PlayerPrefs.Save();
+        }
+
+        else
+        {
+            DeActivatePanel(tutorial);
+        }
+
+    
+       
+    }
+
+     public void ActivatePanel(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+
+    public void DeActivatePanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+
+
      public void mainmenu()
     {
         SceneManager.LoadScene("MenuAndQuizScene");
